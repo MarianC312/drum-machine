fc84521232cbc8fda53035f061200bde4646bdd3
MarianC312 <visual_design@live.com.ar>
2020-06-20 01:08:51 -0300

    primeras adiciones, objeto boton para pad creado.

--- app.jsx
+++ app.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Button extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      text: this.props.text
+    };
+  }
+  render(){
+    return(
+      <div id="" className="drum-pad">
+        {this.state.text}
+      </div>
+    )
+  }
+}
+
 class Main extends React.Component {
   constructor (props) {
     super(props);
@@ -9,8 +25,11 @@ class Main extends React.Component {
 
   render() {
     return  (
-      <div className="container text-center">
-        hello world
+      <div id="drum-machine" className="container text-center">
+        <div id="display">
+        
+        </div>
+         <Button text="Q" />
       </div>
     )
   }
