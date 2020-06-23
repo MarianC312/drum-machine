f8c594af895d17f828a66a19e874c8d546d610de
MarianC312 <visual_design@live.com.ar>
2020-06-23 16:32:44 -0300

    sin mensaje

--- app.jsx
+++ app.jsx
@@ -5,14 +5,49 @@ class Button extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      text: this.props.text
+      text: this.props.text,
+      id: this.props.text
     };
   }
   render(){
     return(
-      <div id="" className="drum-pad">
+      <div id={this.state.id} className="drum-pad btn btn-outline-primary m-1 rounded-0 p-3">
         {this.state.text}
       </div>
+    )
+  }
+}
+
+class Drum extends React.Component{
+  constructor (props){
+    super(props);
+  }
+  
+  render(){
+    return(
+      <div id="drum-machine" className="container text-center">
+              <div id="display">
+              
+              </div>
+              <div className="row">
+                <div className="col-md-4 d-flex">
+                  <Button text="Q" />
+                  <Button text="W" />
+                  <Button text="E" />
+                </div>
+                <div className="col-md-4 d-flex">
+                  <Button text="A" />
+                  <Button text="S" />
+                  <Button text="D" />
+                </div>
+                <div className="col-md-4 d-flex">
+                  <Button text="Z" />
+                  <Button text="X" />
+                  <Button text="C" />
+                </div>
+              </div>
+               
+            </div>
     )
   }
 }
@@ -25,12 +60,14 @@ class Main extends React.Component {
 
   render() {
     return  (
-      <div id="drum-machine" className="container text-center">
-        <div id="display">
-        
-        </div>
-         <Button text="Q" />
-      </div>
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+              <div className="border border-secondary">
+                <div>
+                  <Drum />
+                </div>
+              </div>
+            </div>
+      
     )
   }
 }
